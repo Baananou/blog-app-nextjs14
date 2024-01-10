ed277a08d55bc8644713b0c55ea47f81edb2f892
Baananou <bananoumohamed30@gmail.com>
2024-01-10 14:58:43 +0100

    Haya

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,1 +1,8 @@
 export { GET, POST } from "@/lib/auth"
+
+
+// MONGO= 'mongodb+srv://baananou:baananou@cluster0.49vi6qu.mongodb.net/?retryWrites=true&w=majority'
+// GITHUB_SECRET = 'd84438c5d203440e5f87e71f255b350392c4d5f2'
+// GITHUB_ID = '87dd19af01516041ae39'
+// AUTH_SECRET = 'Qf2q7mJkhvSBPa+ndjJ+aWUeLJ17rv3suwwZb2YLXpE='
+// AUTH_URL = 'http://localhost:3000/api/auth'
